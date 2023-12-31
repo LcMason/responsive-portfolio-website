0c2f7ff733289247bf0e8615aade74aae44b93fc
Lionel Mason <capitalcookhouse@gmail.com>
2023-12-31 14:49:47 -0500

    removes asset/ from files and implement node server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the 'assets' directory
+app.use(express.static(path.join(__dirname, 'assets')));
+
+// Serve the index.html file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
